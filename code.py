24c00fcb25d2d7928ffbc6044c02fc91c4b1c987
Medha-B <52010073+Medha-B@users.noreply.github.com>
2020-03-24 10:54:02 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,15 @@
+pip install liac-arff
+
+from scipy.io import arff
+import pandas as pd
+from io import BytesIO, TextIOWrapper
+from zipfile import ZipFile
+from scipy.io.arff import loadarff
+
+zfile = ZipFile(r"C:\Users\Subroto\Downloads\Breast.zip")
+in_mem_fo = TextIOWrapper(BytesIO(zfile.read('Breast.arff')), encoding='utf-8')
+data = loadarff(in_mem_fo)
+
+df = pd.DataFrame(data[0])
+df.head()
+df.tail()
