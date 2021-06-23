94f1006ae7063180e5f6b1e3c041bb92e71fe521
Medha-B <52010073+Medha-B@users.noreply.github.com>
2021-06-23 15:44:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import find_packages
 
 setup(
     name="Opening-an-arff-type-file-in-Python",
-    packages = find_packages(),
-    version = "1.0.0",
-    license = "Apache",
-    description = "Script for opening ARFF files",
-    author = "Medha-B",
-    url = "https://github.com/user/Medha-B",
-    keywords = ["microarray", "arff"],
-    classifiers = [
+    packages=find_packages(),
+    version="1.0.0",
+    license="Apache",
+    description="Script for opening ARFF files",
+    author="Medha-B",
+    url="https://github.com/user/Medha-B",
+    keywords=["microarray", "arff"],
+    classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
     ]
