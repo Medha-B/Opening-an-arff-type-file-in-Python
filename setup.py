2f7116cdc2c84a3c1b84324f8de483c843ead00f
Medha-B <52010073+Medha-B@users.noreply.github.com>
2021-06-24 17:11:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name="ArffPython",
+    name="Arffpython",
     packages=find_packages(),
     version="1.0.0",
     license="Apache",
