e720f51d0921f67279f9021bb88388452f000f35
Medha-B <52010073+Medha-B@users.noreply.github.com>
2021-06-24 17:21:57 +0530

    Update code.py

--- models/code.py
+++ models/code.py
@@ -1,17 +1,15 @@
-import pandas as pd
-from io import BytesIO, TextIOWrapper
-from zipfile import ZipFile
-from scipy.io.arff import loadarff
+# import pandas as pd
+# from io import BytesIO, TextIOWrapper
+# from zipfile import ZipFile
+# from scipy.io.arff import loadarff
 
-zfile = ZipFile(r"C:\Users\Subroto\Downloads\Breast.zip")
-in_mem_fo = TextIOWrapper(BytesIO(zfile.read('Breast.arff')), encoding='utf-8')
-data = loadarff(in_mem_fo)
+# zfile = ZipFile(r"C:\Users\Subroto\Downloads\Breast.zip")
+# in_mem_fo = TextIOWrapper(BytesIO(zfile.read('Breast.arff')), encoding='utf-8')
+# data = loadarff(in_mem_fo)
 
 
 def plus(a, b):
     return a + b
-
-
-df = pd.DataFrame(data[0])
-df.head()
-df.tail()
+# df = pd.DataFrame(data[0])
+# df.head()
+# df.tail()
